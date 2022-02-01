27f77c1d335a3b65d644f4e12c5e93a8945c1a4f
AnkurS11 <95088240+AnkurS11@users.noreply.github.com>
2022-02-02 01:45:49 +0530

    JSON

--- Angular_JSON.js
+++ Angular_JSON.js
@@ -1,4 +1,13 @@
 var tickets=angular.module("tickets",[]);
+
+tickets.filter('rupees', function() {
+    return function(x) {
+        var i;
+        i = x*74.75;
+        return i;
+    };
+});
+
 tickets.controller("Ticket_ctrl", function($scope,$http)
 {
     $http.get("https://ankurs11.github.io/JSON/data.json")
